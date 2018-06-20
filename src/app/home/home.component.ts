23681609437edf6694bd764746ed3b2f4d6b70f3
Mike Piper <kalindap@iMac.local>
2018-06-20 08:31:42 -0500

    expanded DoB and "already filed" date options to go back further, per a request

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -32,7 +32,8 @@ export class HomeComponent implements OnInit {
   inputMonths: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
   inputDays: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
               16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
-  inputYears = [1938, 1939,
+  inputYears = [1925, 1926, 1927, 1928, 1929,
+              1930, 1931, 1932, 1933, 1934, 1935, 1936, 1937, 1938, 1939,
               1940, 1941, 1942, 1943, 1944, 1945, 1946, 1947, 1948, 1949,
               1950, 1951, 1952, 1953, 1954, 1955, 1956, 1957, 1958, 1959,
               1960, 1961, 1962, 1963, 1964, 1965, 1966, 1967, 1968, 1969,
@@ -42,7 +43,9 @@ export class HomeComponent implements OnInit {
               2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,
               2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018]
 
-  inputBenefitYears = [
+  inputBenefitYears = [1979, //Can't go earlier than 1979 or calculation rules are different. Tough luck to anybody older.
+                    1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989,
+                    1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999,
                     2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,
                     2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019,
                     2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029,
@@ -199,12 +202,12 @@ export class HomeComponent implements OnInit {
     this.spouseBfixedRetirementBenefitDate = new Date(this.spouseBfixedRetirementBenefitYear, this.spouseBfixedRetirementBenefitMonth-1, 1)
 
     this.customSpouseAretirementBenefitDate = new Date(this.customSpouseAretirementBenefitYear, this.customSpouseAretirementBenefitMonth-1, 1)
-      //If spouse A has already filed, there will be no input regarding their retirement date in custom dates form, so go get "fixed" date from above
-      if (this.spouseAhasFiled === true) {this.customSpouseAretirementBenefitDate = new Date(this.spouseAfixedRetirementBenefitDate)}
+        //If spouse A has already filed, there will be no input regarding their retirement date in custom dates form, so go get "fixed" date from above
+        if (this.spouseAhasFiled === true) {this.customSpouseAretirementBenefitDate = new Date(this.spouseAfixedRetirementBenefitDate)}
     this.customSpouseAspousalBenefitDate = new Date(this.customSpouseAspousalBenefitYear, this.customSpouseAspousalBenefitMonth-1, 1)
     this.customSpouseBretirementBenefitDate = new Date(this.customSpouseBretirementBenefitYear, this.customSpouseBretirementBenefitMonth-1, 1)
-          //If spouse B has already filed, there will be no input regarding their retirement date in custom dates form, so go get "fixed" date from above
-          if (this.spouseBhasFiled === true) {this.customSpouseBretirementBenefitDate = new Date(this.spouseBfixedRetirementBenefitDate)}
+        //If spouse B has already filed, there will be no input regarding their retirement date in custom dates form, so go get "fixed" date from above
+        if (this.spouseBhasFiled === true) {this.customSpouseBretirementBenefitDate = new Date(this.spouseBfixedRetirementBenefitDate)}
     this.customSpouseBspousalBenefitDate = new Date(this.customSpouseBspousalBenefitYear, this.customSpouseBspousalBenefitMonth-1, 1)
 
 
