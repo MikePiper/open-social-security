3ed09d2de124632a085907f86c46509e3a4b42b1
Mike Piper <kalindap@iMac.local>
2018-06-22 11:19:50 -0500

    tests created for mortality service

--- src/app/mortality.service.spec.ts
+++ src/app/mortality.service.spec.ts
@@ -14,5 +14,26 @@ describe('MortalityService', () => {
   }));
 
 
-  
+  //check that determineMortalityTable() gets the right table -- check via an indexed value
+  it('should get the correct mortality table', inject([MortalityService], (service: MortalityService) => {
+    let table = service.determineMortalityTable("female", "SSA", 0)
+    expect(table[7])
+        .toEqual(99353)
+  }))
+
+  it('should get the correct mortality table', inject([MortalityService], (service: MortalityService) => {
+    let table = service.determineMortalityTable("male", "NS2", 0)
+    expect(table[50])
+        .toEqual(98691)
+  }))
+
+  //check method for creating mortality table
+  it('should correctly create a mortality table using assumed death age', inject([MortalityService], (service: MortalityService) => {
+    let table = service.createMortalityTable(83)
+    expect(table[82]).toEqual(1)
+    expect(table[83]).toEqual(1)
+    expect(table[84]).toEqual(0)
+  }))
+
+
 });
