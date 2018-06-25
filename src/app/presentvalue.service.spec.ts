ab69deb01eb26a4c7adad1b41d4e427cf3b43ebc
Mike Piper <kalindap@iMac.local>
2018-06-25 17:50:03 -0500

    created a few tests for present value service, still needs several more though

--- src/app/presentvalue.service.spec.ts
+++ src/app/presentvalue.service.spec.ts
@@ -4,12 +4,13 @@ import {BenefitService} from './benefit.service'
 import {EarningsTestService} from './earningstest.service'
 import {SolutionSetService} from './solutionset.service'
 import {MortalityService} from './mortality.service'
+import { BirthdayService } from './birthday.service';
 
 
 describe('PresentValueService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [PresentValueService, BenefitService, EarningsTestService, SolutionSetService, MortalityService]
+      providers: [PresentValueService, BenefitService, EarningsTestService, SolutionSetService, MortalityService, BirthdayService]
     })
   })
 
@@ -48,11 +49,73 @@ describe('PresentValueService', () => {
       .toBeCloseTo(201310, 0)
   }))
   
-  //Test PV calc for married couple, nothing unusual
-  //Check same, including GPO
+  //Test calculateCouplePV
+  it('should return appropriate PV for married couple, basic inputs', inject([PresentValueService], (service: PresentValueService) => { 
+    let maritalStatus:string = "married"
+    let mortalityService:MortalityService = new MortalityService()
+    let spouseAmortalityTable:number[] = mortalityService.determineMortalityTable ("male", "NS2", 0) //Using male nonsmoker2 mortality table
+    let spouseBmortalityTable:number[] = mortalityService.determineMortalityTable ("female", "NS1", 0) //Using female nonsmoker1 mortality table
+    let spouseASSbirthDate: Date = new Date(1964, 8, 1) //Spouse A born in Sept 1959 (has to be under 62 right now, otherwise the value will be different every time we run the calculator because the discounting will happen to a different date)
+    let spouseBSSbirthDate: Date = new Date(1963, 6, 1) //Spouse A born in July 1958
+    let spouseAinitialAgeRounded:number = 61
+    let spouseBinitialAgeRounded:number = 61
+    let birthdayService:BirthdayService = new BirthdayService()
+    let spouseAFRA: Date = birthdayService.findFRA(spouseASSbirthDate)
+    let spouseBFRA: Date = birthdayService.findFRA(spouseBSSbirthDate)
+    let spouseAsurvivorFRA:Date = birthdayService.findSurvivorFRA(spouseASSbirthDate)
+    let spouseBsurvivorFRA:Date = birthdayService.findSurvivorFRA(spouseBSSbirthDate)
+    let spouseAPIA: number = 700
+    let spouseBPIA: number = 1900
+    let spouseAretirementBenefitDate: Date = new Date (2032, 8, 1) //At age 68
+    let spouseBretirementBenefitDate: Date = new Date (2029, 8, 1) //At age 66 and 2 months
+    let spouseAspousalBenefitDate: Date = new Date (2032, 8, 1) //Later of two retirement benefit dates
+    let spouseBspousalBenefitDate: Date = new Date (2032, 8, 1) //Later of two retirement benefit dates
+    let spouseAquitWorkDate: Date = new Date(2018,3,1) //already quit working
+    let spouseBquitWorkDate: Date = new Date(2018,3,1) //already quit working
+    let spouseAmonthlyEarnings: number = 0
+    let spouseBmonthlyEarnings: number = 0
+    let spouseAgovernmentPension: number = 0
+    let spouseBgovernmentPension:number = 0
+    let discountRate:number = 1
+    expect(service.calculateCouplePV(maritalStatus, spouseAmortalityTable, spouseBmortalityTable, spouseASSbirthDate, spouseBSSbirthDate, spouseAinitialAgeRounded, spouseBinitialAgeRounded,
+    spouseAFRA, spouseBFRA, spouseAsurvivorFRA, spouseBsurvivorFRA, spouseAPIA, spouseBPIA, spouseAretirementBenefitDate, spouseBretirementBenefitDate, spouseAspousalBenefitDate, spouseBspousalBenefitDate,
+    spouseAquitWorkDate, spouseBquitWorkDate, spouseAmonthlyEarnings, spouseBmonthlyEarnings, spouseAgovernmentPension, spouseBgovernmentPension, discountRate))
+      .toBeCloseTo(578594, 0)
+  }))
 
-  //Test PV calc for "one has filed" function, married
-  //Test PV calc for "one has filed" function, divorced
+  /*
+  it ('should return appropriate PV for married couple, including GPO', inject([PresentValueService], (service: PresentValueService) => {
+    //params
+    expect(service.calculateCouplePV(maritalStatus, spouseAmortalityTable, spouseBmortalityTable, spouseASSbirthDate, spouseBSSbirthDate, spouseAinitialAgeRounded, spouseBinitialAgeRounded,
+    spouseAFRA, spouseBFRA, spouseAsurvivorFRA, spouseBsurvivorFRA, spouseAPIA, spouseBPIA, spouseAretirementBenefitDate, spouseBretirementBenefitDate, spouseAspousalBenefitDate, spouseBspousalBenefitDate,
+    spouseAquitWorkDate, spouseBquitWorkDate, spouseAmonthlyEarnings, spouseBmonthlyEarnings, spouseAgovernmentPension, spouseBgovernmentPension, discountRate))
+      .toBeCloseTo(99999, 0)
+  }))
 
-  //Ways to test maximize functions??
+  it ('should return appropriate PV for married couple, one has already filed', inject([PresentValueService], (service: PresentValueService) => {
+    //params
+    expect(service.calculateCouplePV(maritalStatus, spouseAmortalityTable, spouseBmortalityTable, spouseASSbirthDate, spouseBSSbirthDate, spouseAinitialAgeRounded, spouseBinitialAgeRounded,
+    spouseAFRA, spouseBFRA, spouseAsurvivorFRA, spouseBsurvivorFRA, spouseAPIA, spouseBPIA, spouseAretirementBenefitDate, spouseBretirementBenefitDate, spouseAspousalBenefitDate, spouseBspousalBenefitDate,
+    spouseAquitWorkDate, spouseBquitWorkDate, spouseAmonthlyEarnings, spouseBmonthlyEarnings, spouseAgovernmentPension, spouseBgovernmentPension, discountRate))
+      .toBeCloseTo(99999, 0)
+  }))
+
+  it ('should return appropriate PV for married couple, one has already filed (same as above, except A and B are swapped)', inject([PresentValueService], (service: PresentValueService) => {
+    //params
+    expect(service.calculateCouplePV(maritalStatus, spouseAmortalityTable, spouseBmortalityTable, spouseASSbirthDate, spouseBSSbirthDate, spouseAinitialAgeRounded, spouseBinitialAgeRounded,
+    spouseAFRA, spouseBFRA, spouseAsurvivorFRA, spouseBsurvivorFRA, spouseAPIA, spouseBPIA, spouseAretirementBenefitDate, spouseBretirementBenefitDate, spouseAspousalBenefitDate, spouseBspousalBenefitDate,
+    spouseAquitWorkDate, spouseBquitWorkDate, spouseAmonthlyEarnings, spouseBmonthlyEarnings, spouseAgovernmentPension, spouseBgovernmentPension, discountRate))
+      .toBeCloseTo(99999, 0)
+  }))
+
+  it ('should return appropriate PV for divorcee scenario', inject([PresentValueService], (service: PresentValueService) => {
+    //params
+    expect(service.calculateCouplePV(maritalStatus, spouseAmortalityTable, spouseBmortalityTable, spouseASSbirthDate, spouseBSSbirthDate, spouseAinitialAgeRounded, spouseBinitialAgeRounded,
+    spouseAFRA, spouseBFRA, spouseAsurvivorFRA, spouseBsurvivorFRA, spouseAPIA, spouseBPIA, spouseAretirementBenefitDate, spouseBretirementBenefitDate, spouseAspousalBenefitDate, spouseBspousalBenefitDate,
+    spouseAquitWorkDate, spouseBquitWorkDate, spouseAmonthlyEarnings, spouseBmonthlyEarnings, spouseAgovernmentPension, spouseBgovernmentPension, discountRate))
+      .toBeCloseTo(99999, 0)
+  }))
+  */
+
+  //Test maximize functions
 })
