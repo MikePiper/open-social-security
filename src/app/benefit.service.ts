490ecd54c80a2e2fc9bc6a5b63b35ca723fbbf21
Brian Courts <45345404+brian-courts@users.noreply.github.com>
2018-12-13 15:32:42 -0600

    WEP changes
    Revised calculateSurvivorBenefit function to calculate benefit to survivor of a beneficiary affected by WEP

--- src/app/benefit.service.ts
+++ src/app/benefit.service.ts
@@ -78,54 +78,79 @@ export class BenefitService {
   calculateSurvivorBenefit(survivingPerson:Person, survivorRetirementBenefit: number,  survivorSurvivorBenefitDate: MonthYearDate,
     deceasedPerson:Person, dateOfDeath: MonthYearDate, deceasedClaimingDate: MonthYearDate)
   {
-    let deceasedRetirementBenefit: number
+    let deceasedRetirementBenefit: number = 0 // 
     let survivorBenefit: number
 
-    //If deceased had filed, survivorBenefit = deceased spouse's retirement benefit, but no less than 82.5% of deceased's PIA
+    let survivorBenefitPIA: number // PIA to use for calculating survivor benefits
+    let wepAdjustmentFactor: number
+    let pia825: number // can set lower limit of survivor benefit
+    let wepAffected: boolean // deceased was affected by Windfall Elimination Provision
+
+    if (deceasedPerson.wepSurvivorPIA == 0) {
+      // deceasedPerson was not affected by WEP
+      survivorBenefitPIA = deceasedPerson.PIA
+      wepAdjustmentFactor = 1
+      wepAffected = false
+    } else {
+      // deceasedPerson was affected by WEP
+      survivorBenefitPIA = deceasedPerson.wepSurvivorPIA
+      wepAdjustmentFactor = survivorBenefitPIA / deceasedPerson.PIA
+      wepAffected = true
+    }
+    pia825 = 0.825 * survivorBenefitPIA
+
+    //If deceasedPerson had filed, survivorBenefit = deceasedPerson's retirement benefit (with no WEP reduction), but no less than 82.5% of deceased's PIA
     if (deceasedClaimingDate <= dateOfDeath) {
       deceasedRetirementBenefit = this.calculateRetirementBenefit(deceasedPerson, deceasedClaimingDate)
-      survivorBenefit = deceasedRetirementBenefit
-      if (survivorBenefit < 0.825 * deceasedPerson.PIA) {
-        survivorBenefit = 0.825 * deceasedPerson.PIA
-       }
+      if (wepAffected) {
+        deceasedRetirementBenefit = deceasedRetirementBenefit * wepAdjustmentFactor
       }
-    else { //i.e., if deceased sposue had NOT filed as of date of death...
-        //if deceased spouse was younger than FRA, survivor benefit = deceasedPIA
-        if (dateOfDeath < deceasedPerson.FRA){
-          survivorBenefit = deceasedPerson.PIA
-        }
-        //if deceased spouse was older than FRA, survivorBenefit = deceased's retirement benefit on date of death
-        else {
-        survivorBenefit = this.calculateRetirementBenefit(deceasedPerson, dateOfDeath)
-        }
+
+      survivorBenefit = deceasedRetirementBenefit
+      if (survivorBenefit < pia825) {
+        survivorBenefit = pia825
+      }
     }
-    
+    else { //i.e., deceasedPerson had NOT filed as of date of death...
+      //if deceasedPerson was younger than FRA, survivor benefit = deceasedPerson's survivorBenefitPIA
+      if (dateOfDeath < deceasedPerson.FRA) {
+        survivorBenefit = survivorBenefitPIA
+      }
+      //if deceased spouse was older than FRA, survivorBenefit = retirement benefit deceased could have received on date of death, adjusted if reduced for WEP
+      else {
+        survivorBenefit = this.calculateRetirementBenefit(deceasedPerson, dateOfDeath)
+        if (wepAffected) {
+          survivorBenefit = survivorBenefit * wepAdjustmentFactor
+        }
+      }
+    }
+
     //If survivor files for survivor benefit prior to their survivorFRA...
-    if (survivorSurvivorBenefitDate < survivingPerson.survivorFRA){
+    if (survivorSurvivorBenefitDate < survivingPerson.survivorFRA) {
 
       //find percentage of the way survivor is from 60 to FRA
-      let monthsFrom60toFRA: number = (survivingPerson.survivorFRA.getFullYear() - (survivingPerson.SSbirthDate.getFullYear()+60))*12 + (survivingPerson.survivorFRA.getMonth() - survivingPerson.SSbirthDate.getMonth())
-      let monthsElapsed: number = (survivorSurvivorBenefitDate.getFullYear() - (survivingPerson.SSbirthDate.getFullYear()+60))*12 + (survivorSurvivorBenefitDate.getMonth() - survivingPerson.SSbirthDate.getMonth())
+      let monthsFrom60toFRA: number = (survivingPerson.survivorFRA.getFullYear() - (survivingPerson.SSbirthDate.getFullYear() + 60)) * 12 + (survivingPerson.survivorFRA.getMonth() - survivingPerson.SSbirthDate.getMonth())
+      let monthsElapsed: number = (survivorSurvivorBenefitDate.getFullYear() - (survivingPerson.SSbirthDate.getFullYear() + 60)) * 12 + (survivorSurvivorBenefitDate.getMonth() - survivingPerson.SSbirthDate.getMonth())
       let percentageWaited: number = monthsElapsed / monthsFrom60toFRA
 
-      //if deceased had not filed before FRA, adjust survivor benefit downward relative to initial survivor benefit calculation above.
-      if (deceasedClaimingDate >= deceasedPerson.FRA) {
+      if (deceasedClaimingDate >= deceasedPerson.FRA) { //deceased had not filed before FRA
+        //adjust survivor benefit downward relative to initial survivor benefit calculation above.
         survivorBenefit = survivorBenefit - (survivorBenefit * 0.285 * (1 - percentageWaited))
-      }
-
-      //If deceased had filed before FRA, do completely new calculation, with survivor benefit based on deceasedPIA rather than deceased retirement benefit.
-      if (deceasedClaimingDate < deceasedPerson.FRA && survivorSurvivorBenefitDate < survivingPerson.survivorFRA) {
-        survivorBenefit = deceasedPerson.PIA - (deceasedPerson.PIA * 0.285 * (1 - percentageWaited))
-        //survivorBenefit then limited to greater of 82.5% of deceased's PIA or amount deceased was receiving on date of death
-        if (0.825 * deceasedPerson.PIA < deceasedRetirementBenefit) {
+      } else { //deceased had filed before FRA
+        // do completely new calculation, with survivor benefit based on survivorBenefitPIA rather than deceased retirement benefit.
+        survivorBenefit = survivorBenefitPIA - (survivorBenefitPIA * 0.285 * (1 - percentageWaited))
+        //survivorBenefit then limited to greater of
+        // (A) 82.5 % of deceased's PIA or
+        // (B) amount deceased was receiving on date of death or would have received, if not affected by WEP
+        if (pia825 < deceasedRetirementBenefit) {
           if (survivorBenefit > deceasedRetirementBenefit) {
             survivorBenefit = deceasedRetirementBenefit
           }
         } else {
-            if (survivorBenefit > 0.825 * deceasedPerson.PIA) {
-              survivorBenefit = 0.825 * deceasedPerson.PIA
-            }
+          if (survivorBenefit > pia825) {
+            survivorBenefit = pia825
           }
+        }
       }
     }
       //subtract own retirement benefit
