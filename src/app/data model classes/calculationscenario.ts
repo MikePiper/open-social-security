9c68f10e0a74d40fe8c9722f845bd655d6d68652
Mike Piper <kalindap@iMac.local>
2018-12-20 09:32:04 -0600

    setChildrenArray() sets youngestChildTurns16date & disabledChild fields

--- src/app/data model classes/calculationscenario.ts
+++ src/app/data model classes/calculationscenario.ts
@@ -8,6 +8,8 @@ export class CalculationScenario {
     maritalStatus: string = "single"
     numberOfChildren:number = 0
     children:Person[] = []
+    youngestChildTurns16date:MonthYearDate
+    disabledChild:boolean = false
     benefitCutAssumption: boolean = false
         benefitCutYear: number = 2034
         benefitCutPercentage: number = 23
@@ -17,14 +19,36 @@ export class CalculationScenario {
 
     setChildrenArray(childrenArray:Person[], today:MonthYearDate){
         this.children = []
-        for (let child of childrenArray){
-            this.children.push(child)
-            child.age = (today.getMonth() - child.SSbirthDate.getMonth() + 12 * (today.getFullYear() - child.SSbirthDate.getFullYear()) )/12
+        //Have to do it this wonky way using scenario.numberOfChildren, because childrenArray always has 4 objects in it, even if there are no actual children.
+        for (let i:number = 0; i < this.numberOfChildren; i++){ 
+            this.children.push(childrenArray[i])
         }
-        //sort children by age
-        this.children.sort(function(a,b){
-            return a.age - b.age
-        })
+
+        setTimeout( () => {//Need to have a timeout here, because we have to let the getinputs() function from the childinputs component finish running, otherwise child objects have no SSbirthdate fields
+            //calculate ages of children
+            for (let child of this.children){
+                child.age = (today.getMonth() - child.SSbirthDate.getMonth() + 12 * (today.getFullYear() - child.SSbirthDate.getFullYear()) )/12
+            }
+
+            //calculate youngestChildTurns16date
+            let latestSSbirthdate:MonthYearDate = this.children[0].SSbirthDate
+            for (let child of this.children){
+                if (child.SSbirthDate > latestSSbirthdate){
+                    latestSSbirthdate = new MonthYearDate(child.SSbirthDate)
+                }
+            }
+            this.youngestChildTurns16date = new MonthYearDate(latestSSbirthdate.getFullYear()+16, latestSSbirthdate.getMonth())
+
+            //set disabledChild boolean
+            this.disabledChild = false
+            for (let child of childrenArray){
+                if (child.isOnDisability === true){
+                    this.disabledChild = true
+                }
+            }
+            console.log("youngestChildTurns16date: " + Number(this.youngestChildTurns16date.getMonth()+1) + "/" + this.youngestChildTurns16date.getFullYear())
+            console.log("disabledChild: " + this.disabledChild)
+        }, 25)
     }
 }
 
