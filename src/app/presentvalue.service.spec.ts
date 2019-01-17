3d9303afab317419642f1fae19b78886600260ec
Mike Piper <kalindap@iMac.local>
2019-01-17 13:35:11 -0600

    new tests to check survivor benefit calculated using nonWEP PIA when applicable

--- src/app/presentvalue.service.spec.ts
+++ src/app/presentvalue.service.spec.ts
@@ -869,6 +869,62 @@ describe('tests calculateCouplePV', () => {
         expect(scenario.outputTable[6][6]).toEqual("$0") //deceased filed at 70 with FRA of 67. Benefit would have been 1240. Minus survivor's own 1500 retirement benefit, gives zero survivor benefit
       })
 
+      //Testing calculation of survivor benefits in scenario where deceased was affected by Windfall Elimination Provision
+        it('should calculate WEP-affected survivor benefit appropriately after FRA, with own smaller retirement benefit. Deceased personA filed at age 70', () => {
+          scenario.maritalStatus = "married"
+          scenario.discountRate = 1
+          personA.mortalityTable = mortalityService.determineMortalityTable ("male", "SSA", 0)
+          personB.mortalityTable = mortalityService.determineMortalityTable ("female", "SSA", 0)
+          personA.actualBirthDate = new Date(1963, 2, 15) //March 1963
+          personB.actualBirthDate = new Date(1963, 7, 2) //August 1963
+          personA.SSbirthDate = new MonthYearDate(1963, 2)
+          personB.SSbirthDate = new MonthYearDate (1963, 7)
+          personA.FRA = birthdayService.findFRA(personA.SSbirthDate) //March 2030
+          personB.FRA = birthdayService.findFRA(personB.SSbirthDate) //August 2030
+          personA.survivorFRA = birthdayService.findSurvivorFRA(personA.SSbirthDate)
+          personB.survivorFRA = birthdayService.findSurvivorFRA(personB.SSbirthDate)
+          personA.PIA = 1000
+          personB.PIA = 1000
+          personA.receivesNonCoveredPension = true
+          personA.nonWEP_PIA = 1200
+          personA.retirementBenefitDate = new MonthYearDate(2033, 2) //March 2033, age 70
+          personB.retirementBenefitDate = new MonthYearDate(2025, 7) //Files at 62 (5 years before FRA), so retirement benefit = 700
+          personA.spousalBenefitDate = new MonthYearDate(2033, 2) //later of two retirementBenefitDates
+          personB.spousalBenefitDate = new MonthYearDate(2033, 2) //later of two retirementBenefitDates
+          service.calculateCouplePV(personA, personB, scenario, true)
+          expect(scenario.outputTable[11][0]).toEqual("If your spouse outlives you")
+          expect(scenario.outputTable[11][6]).toEqual("$9,456")
+          //deceased filed at 70 with FRA of 67. Benefit would have been 1488, given nonWEP PIA of 1200.
+          //Minus survivor's own 700 retirement benefit, gives 788 survivor benefit. 788 x 12 = 9456
+        })
+
+        it('should calculate WEP-affected survivor benefit appropriately as zero with own larger retirement benefit. Deceased filed at age 70', () => {
+          scenario.maritalStatus = "married"
+          scenario.discountRate = 1
+          personA.mortalityTable = mortalityService.determineMortalityTable ("male", "SSA", 0)
+          personB.mortalityTable = mortalityService.determineMortalityTable ("female", "SSA", 0)
+          personA.actualBirthDate = new Date(1963, 2, 15) //March 1963
+          personB.actualBirthDate = new Date(1963, 7, 2) //August 1963
+          personA.SSbirthDate = new MonthYearDate(1963, 2)
+          personB.SSbirthDate = new MonthYearDate (1963, 7)
+          personA.FRA = birthdayService.findFRA(personA.SSbirthDate) //March 2030
+          personB.FRA = birthdayService.findFRA(personB.SSbirthDate) //August 2030
+          personA.survivorFRA = birthdayService.findSurvivorFRA(personA.SSbirthDate)
+          personB.survivorFRA = birthdayService.findSurvivorFRA(personB.SSbirthDate)
+          personA.PIA = 1000
+          personB.PIA = 1500
+          personA.receivesNonCoveredPension = true
+          personA.nonWEP_PIA = 1200
+          personA.retirementBenefitDate = new MonthYearDate(2033, 2) //March 2033, age 70
+          personB.retirementBenefitDate = new MonthYearDate(personB.FRA) //Files at FRA. retirement benefit = 1500
+          personA.spousalBenefitDate = new MonthYearDate(2033, 2) //later of two retirementBenefitDates
+          personB.spousalBenefitDate = new MonthYearDate(2033, 2) //later of two retirementBenefitDates
+          service.calculateCouplePV(personA, personB, scenario, true)
+          expect(scenario.outputTable[6][0]).toEqual("If your spouse outlives you")
+          expect(scenario.outputTable[6][6]).toEqual("$0")
+            //deceased filed at 70 with FRA of 67. Benefit ignoring WEP would have been 1488, given nonWEP PIA of 1200. Minus survivor's own 1500 retirement benefit, gives zero survivor benefit
+        })
+
       it('should appropriately reflect personB spousal benefit being partially withheld based on personA excess earnings', () => {
         service.today = new MonthYearDate(2018, 11) //Test was written in 2018. Have to hardcode in the year, otherwise it will fail every new year.
         personA.actualBirthDate = new Date(1956, 5, 10) //born June 1956
