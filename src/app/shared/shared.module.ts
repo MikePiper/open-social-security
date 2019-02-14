4a2cb0620d000e01f09c91f47597f098c85942bc
Mike Piper <kalindap@iMac.local>
2019-02-14 13:28:54 -0600

    articles can now have proper html titles via setTitle in their constructor

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Title }  from '@angular/platform-browser';
 import { BookPromoComponent } from './book-promo/book-promo.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   exports: [BookPromoComponent],
-  declarations: [BookPromoComponent]
+  declarations: [BookPromoComponent],
+  providers: [Title]
 })
 export class SharedModule { }
