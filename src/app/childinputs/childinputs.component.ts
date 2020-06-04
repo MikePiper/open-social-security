bbc6d7f00c82ba48e9e4367ed8c8dc0796a18252
Mike Piper <kalindap@iMac.local>
2020-06-04 08:59:42 -0500

    updated range of child DoB inputs

--- src/app/childinputs/childinputs.component.ts
+++ src/app/childinputs/childinputs.component.ts
@@ -33,11 +33,12 @@ export class ChildinputsComponent implements OnInit {
               1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989,
               1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999,
               2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,
-              2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018]
+              2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019,
+              2020]
 
   DoBinputMonth:number = 4
   DoBinputDay:number = 15
-  DoBinputYear:number = 2000
+  DoBinputYear:number = 2010
 
 
   getInputs(){
