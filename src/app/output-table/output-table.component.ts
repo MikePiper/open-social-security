80522554a3f46e9462eca30bc0a4e7c0c3c26754
Mike Piper <kalindap@iMac.local>
2020-09-06 16:45:26 -0500

    udpates to output table component for survivor case

--- src/app/output-table/output-table.component.ts
+++ src/app/output-table/output-table.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core'
 import { CalculationScenario } from '../data model classes/calculationscenario'
 import { Person } from '../data model classes/person'
 import { ClaimStrategy } from '../data model classes/claimStrategy'
+import { BirthdayService } from '../birthday.service'
 
 @Component({
   selector: 'app-output-table',
@@ -10,7 +11,7 @@ import { ClaimStrategy } from '../data model classes/claimStrategy'
 })
 export class OutputTableComponent implements OnInit {
 
-  constructor() { }
+  constructor(private birthdayService:BirthdayService) { }
 
   ngOnInit() {
   }
