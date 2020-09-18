98edac9ab6a1d71ccee12370b3c373dec277a5cd
Mike Piper <kalindap@iMac.local>
2020-09-18 14:29:43 -0500

    changes to karma config so tests can run without disconnect error

--- karma.conf.js
+++ karma.conf.js
@@ -28,9 +28,11 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false, // with the original karma.conf.js, was doing multiple runs in random order, but the tests kept restarting on my computer (when "disconnected"?)
-    // singleRun: true, // so I added these two lines
-    // random: false, // still doesn't run all tests
-    browserNoActivityTimeout : 60000//by default 10000
+    singleRun: false,
+      // with the original karma.conf.js, was doing multiple runs in random order, but the tests kept restarting on my computer (when "disconnected"?)
+      // singleRun: true, // so I added these two lines
+      // random: false, // still doesn't run all tests
+    browserNoActivityTimeout : 100000,//by default 10000
+    browserDisconnectTimeout: 10000//by default 2000
   });
 };
