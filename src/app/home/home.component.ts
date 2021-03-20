adb8cdd52cbd69682b60f03759fa9c1f28d6ed6a
MikePiper <michaelrpiper@gmail.com>
2021-03-20 09:05:11 -0500

    added 2021 to input years for DoB or DoD

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -105,7 +105,7 @@ export class HomeComponent implements OnInit {
               1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999,
               2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,
               2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019,
-              2020]
+              2020, 2021]
 
   inputBenefitYears: number[] = [1979, //Can't go earlier than 1979 or calculation rules are different. Tough luck to anybody older.
                     1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989,
